c8463ae854e394a8286743c092463345df584661
Jake Shilling <shilling.jake@gmail.com>
2019-07-15 08:34:27 -0400

    Adding user account related routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Profile from "./components/Profile";
 import NotFound from "./components/NotFound";
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/profile" component={Profile} />
         <Route component={NotFound} />
       </Switch>
     </Router>
