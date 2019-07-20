24ad4c643db2d9964942d9bad48105e83a04a61a
Jake Shilling <shilling.jake@gmail.com>
2019-07-20 11:36:02 -0400

    Validating form input

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Button, Form, Input } from "semantic-ui-react";
+import {
+  Divider,
+  Message,
+  Label,
+  Button,
+  Form,
+  Input
+} from "semantic-ui-react";
 
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -7,38 +14,84 @@ class Signup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
-      email: "",
-      password: "",
-      confirmPassword: ""
+      data: {
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+      },
+      errors: {
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+      },
+      canSubmit: false
     };
   }
 
   handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+    const data = { ...this.state.data, [e.target.name]: e.target.value };
+    let errors = {};
 
-  emailErrors = () => {
-    if (this.state.email.length === 0) {
-      return "Required";
-    } else if (!EMAIL_REGEX.test(this.state.email)) {
-      return "Invalid email";
+    errors.name = "";
+
+    if (data.email && !EMAIL_REGEX.test(data.email)) {
+      errors.email = "Please enter a valid email address";
+    } else {
+      errors.email = "";
     }
-    return null;
+
+    if (data.password && data.password.length < 8) {
+      errors.password = "Password should be at least 8 characters";
+    } else if (data.password && !/[A-Z|a-z]/.test(data.password)) {
+      errors.password = "Password should contain at least one letter";
+    } else if (data.password && !/[0-9]/.test(data.password)) {
+      errors.password = "Password should contain at least one number";
+    } else if (
+      data.password &&
+      !/[~|!|@|#|$|%|^|&|*|_|\-|+|=|`|\||\\|\(|\)|{|}|\[|\]|:|;|"|'|<|>|,|\.|\?|\/]/.test(
+        data.password
+      )
+    ) {
+      errors.password =
+        "Password should contain at least one of the following special characters: (~!@#$%^&*_-+=`|(){}[]:;\"'<>,.?/)";
+    } else {
+      errors.password = "";
+    }
+
+    if (data.confirmPassword && data.password !== data.confirmPassword) {
+      errors.confirmPassword = "Passwords do not match";
+    } else {
+      errors.confirmPassword = "";
+    }
+
+    const canSubmit =
+      data.name &&
+      data.email &&
+      data.password &&
+      data.confirmPassword &&
+      !errors.name &&
+      !errors.email &&
+      !errors.password &&
+      !errors.confirmPassword;
+
+    this.setState({
+      data,
+      errors,
+      canSubmit
+    });
   };
 
   passwordErrors = () => {
-    if (this.state.password.length === 0) {
-      return "Required";
-    }
-
     return null;
   };
 
   confirmPasswordErrors = () => {
-    if (this.state.confirmPassword.length === 0) {
-      return "Required";
-    } else if (this.state.password !== this.state.confirmPassword) {
+    if (
+      this.state.confirmPassword &&
+      this.state.password !== this.state.confirmPassword
+    ) {
       return "Does not match";
     }
 
@@ -46,38 +99,55 @@ class Signup extends React.Component {
   };
 
   render() {
+    const errors = Object.values(this.state.errors).filter(
+      message => !!message
+    );
+
     return (
-      <Form>
+      <Form error={errors.length > 0}>
+        <Message
+          error
+          header="Please address the following problems:"
+          list={errors}
+        />
         <Form.Input
           name="name"
           placeholder="Name"
-          value={this.state.name}
+          label="Display Name"
+          value={this.state.data.name}
           onChange={this.handleInputChange}
+          error={!!this.state.errors.name}
         />
+        <Divider />
         <Form.Input
           name="email"
-          placeholder="email"
-          value={this.state.email}
+          placeholder="Email"
+          label="Email Address"
+          value={this.state.data.email}
           onChange={this.handleInputChange}
-          error={this.emailErrors()}
+          error={!!this.state.errors.email}
         />
         <Form.Input
           name="password"
           type="password"
           placeholder="Password"
-          value={this.state.password}
+          label="Enter Password"
+          value={this.state.data.password}
           onChange={this.handleInputChange}
-          error={this.passwordErrors()}
+          error={!!this.state.errors.password}
         />
         <Form.Input
           name="confirmPassword"
           type="password"
           placeholder="Confirm Password"
-          value={this.state.confirmPassword}
+          label="Confirm Password"
+          value={this.state.data.confirmPassword}
           onChange={this.handleInputChange}
-          error={this.confirmPasswordErrors()}
+          error={!!this.state.errors.confirmPassword}
         />
-        <Button type="submit">Submit</Button>
+        <Button disabled={!this.state.canSubmit} type="submit">
+          Submit
+        </Button>
       </Form>
     );
   }
