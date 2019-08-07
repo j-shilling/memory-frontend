974c14039607dca41b11d87950636a4b3e48139d
Jake Shilling <shilling.jake@gmail.com>
2019-08-07 08:21:33 -0400

    DRYing up the tests

--- src/components/Signup.test.js
+++ src/components/Signup.test.js
@@ -4,82 +4,43 @@ import { shallow, mount } from "enzyme";
 import Signup from "./Signup";
 
 describe("<Signup />", () => {
-  const wrapper = mount(<Signup />);
-  const initState = wrapper.state();
   const mockData = {
     name: "New Name",
     email: "new.name@email.com",
     password: "p@ssw0rd",
     confirmPassword: "p@ssw0rd"
   };
-  describe("Inputs are rendered", () => {
-    it("renders one form element", () => {
-      expect(wrapper).toContainExactlyOneMatchingElement("form");
+  describe("User sees inputs", () => {
+    const wrapper = shallow(<Signup />);
+    it("User sees a form element", () => {
+      expect(wrapper).toContainExactlyOneMatchingElement("Form");
     });
-    it("renders name input element", () => {
-      expect(wrapper).toContainExactlyOneMatchingElement("input[name='name']");
-    });
-    it("renders email input element", () => {
-      expect(wrapper).toContainExactlyOneMatchingElement("input[name='email']");
-    });
-    it("renders password input element", () => {
+    const testField = field => {
       expect(wrapper).toContainExactlyOneMatchingElement(
-        "input[name='password']"
+        `FormInput[name="${field}"]`
       );
-    });
-    it("renders confirm password input element", () => {
-      expect(wrapper).toContainExactlyOneMatchingElement(
-        "input[name='confirmPassword']"
-      );
-    });
+    };
+    for (let field in mockData) {
+      it(`User sees a ${field} input`, () => {
+        testField(field);
+      });
+    }
   });
-  describe("Inputs change state", () => {
-    wrapper.setState(initState);
+  describe("User can enter values", () => {
+    const wrapper = shallow(<Signup />);
     const getMockEvent = field => {
       return { target: { name: field, value: mockData[field] } };
     };
     const testField = field => {
-      const data = wrapper.state("data");
-      wrapper
-        .find(`input[name='${field}']`)
-        .simulate("change", getMockEvent(field));
-      expect(wrapper).toHaveState({
-        data: { ...data, [field]: mockData[field] }
+      const input = () => wrapper.find(`FormInput[name="${field}"]`).first();
+      input().simulate("change", getMockEvent(field));
+      expect(input().prop("value")).toEqual(mockData[field]);
+    };
+
+    for (let field in mockData) {
+      it(`User can change ${field} input`, () => {
+        testField(field);
       });
-    };
-
-    it("name input element changes state", () => {
-      testField("name");
-    });
-    it("email input element changes state", () => {
-      testField("email");
-    });
-    it("password input element changes state", () => {
-      testField("password");
-    });
-    it("confirm password input element changes state", () => {
-      testField("confirmPassword");
-    });
-  });
-  describe("State changes input values", () => {
-    wrapper.setState({ ...initState, data: mockData });
-    const testField = field => {
-      expect(wrapper.find(`input[name="${field}"]`).prop("value")).toEqual(
-        mockData[field]
-      );
-    };
-
-    it("state changes name input element", () => {
-      testField("name");
-    });
-    it("state changes email input element", () => {
-      testField("email");
-    });
-    it("state changes password input element", () => {
-      testField("password");
-    });
-    it("state changes confirm password input element", () => {
-      testField("confirmPassword");
-    });
+    }
   });
 });
